8c7729a9c4247b1d6b7c0adc33fc517d71844209
Rafael Ross <rafaellross@gmail.com>
2023-03-07 17:39:08 -0300

    Adicionado a funcao de download para extrado de transacoes por student

--- src/main/java/com/fiap/spring/students/controller/TransactionController.java
+++ src/main/java/com/fiap/spring/students/controller/TransactionController.java
@@ -1,15 +1,26 @@
 package com.fiap.spring.students.controller;
 
 import com.fiap.spring.students.dto.TransactionDTO;
+import com.fiap.spring.students.entities.Student;
 import com.fiap.spring.students.entities.Transaction;
+import com.fiap.spring.students.mappers.StudentMapper;
+import com.fiap.spring.students.repositories.StudentRepository;
+import com.fiap.spring.students.services.StudentService;
 import com.fiap.spring.students.services.TransactionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -21,6 +32,8 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @Autowired
+    private StudentService studentService;
     @PostMapping
     @ApiOperation(value="this method creates a new transaction")
     public ResponseEntity<TransactionDTO> createTransaction(@RequestBody TransactionDTO transactionDTO) {
@@ -39,5 +52,31 @@ public class TransactionController {
     public ResponseEntity<List<Transaction>> getTransactionsByStudentId(@PathVariable Long studentId) {
         List<Transaction> transactions = transactionService.getTransactionsByStudentId(studentId);
         return ResponseEntity.ok(transactions);
+    }
+
+    @GetMapping("/{studentId}/report")
+    public ResponseEntity<byte[]> createTransactionReport(@PathVariable Long studentId) throws IOException {
+
+        List<Transaction> transactionList = transactionService.getTransactionsByStudentId(studentId);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+        writer.write("ID,Card Number,Value, Type, Date\n");
+        for (Transaction transaction : transactionList) {
+            writer.write(String.format("%d,%s,%.2f, %s, %s", transaction.getId(), transaction.getCardNumber(), transaction.getAmount(), transaction.getType(), transaction.getDate()));
+            writer.newLine();
+        }
+
+        writer.flush();
+        writer.close();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=transactions_student_%s.csv", studentId));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(outputStream.toByteArray().length)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(outputStream.toByteArray());
+
     }
 }
